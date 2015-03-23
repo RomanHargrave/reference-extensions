bec35cba3163eb5a8b181168491be85279584a46
Roman Hargrave <roman@hargrave.info>
2015-03-23 18:47:43 -0500

    document implicit option->boolean converter

--- src/main/scala/info/hargrave/refext/OptionAsBoolean.scala
+++ src/main/scala/info/hargrave/refext/OptionAsBoolean.scala
@@ -4,6 +4,14 @@ package info.hargrave.refext
  * Provides an implicit conversion from [[Option]] to [[Boolean]]
  */
 trait OptionAsBoolean {
+
+    /**
+     * Implicit converter that will convert an expression of `Option[_]` to a boolean value corresponding to [[Option.isDefined]]
+     *
+     * @param opt   option expression
+     * @return      result of [[Option.isDefined]]
+     */
     implicit def option2boolean(opt: Option[Any]): Boolean = opt.isDefined
+
 }
 object OptionAsBoolean extends AnyRef with OptionAsBoolean
