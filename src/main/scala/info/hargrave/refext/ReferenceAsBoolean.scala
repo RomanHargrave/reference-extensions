3d70808f150a35703cedfad80b16bb2ef2ea7aa0
Roman Hargrave <roman@hargrave.info>
2015-03-23 18:38:38 -0500

    Document ReferenceAsBoolean

--- src/main/scala/info/hargrave/refext/ReferenceAsBoolean.scala
+++ src/main/scala/info/hargrave/refext/ReferenceAsBoolean.scala
@@ -1,11 +1,16 @@
-package info.hargrave.refext;
+package info.hargrave.refext
 
 /**
- * Date: 3/22/15
- * Time: 6:30 PM
+ * Provides an implicit conversion from [[AnyRef]] to [[Boolean]] depending on whether or not said reference is null
  */
 trait ReferenceAsBoolean {
 
+    /**
+     * Implicit converter from AnyRef to Boolean, which will convert AnyRef to true if it is non-null, otherwise to false
+     *
+     * @param ref   reference to convert to Boolean
+     * @return      whether or not `ref` is null
+     */
     implicit def IsValid(ref: AnyRef): Boolean = ref != null
 
 }
